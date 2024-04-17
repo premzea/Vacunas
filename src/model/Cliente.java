a77d19b78ca6a7649948a9303620b7caa33dbb2e
premzea <prem.zea@u.icesi.edu.co>
2024-04-17 08:22:33 -0500

    Started

---
+++ src/model/Cliente.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Cliente{
+    private String nombre;
+    private String apellido;
+    private String id;
+    private int edad;
+    private Carnet carnet;
+    
+}
